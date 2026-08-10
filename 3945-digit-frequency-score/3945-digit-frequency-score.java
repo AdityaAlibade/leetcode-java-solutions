dbdea8384c70374b216c2164656a318a12cd066c
Aditya Alibade <alibadeaditya@gmail.com>
2026-08-10 19:54:54 +0530

    Time: 1 ms (98.35%), Space: 42.3 MB (92.25%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.java
+++ 3945-digit-frequency-score/3945-digit-frequency-score.java
@@ -1,10 +1,8 @@
 class Solution {
     public int digitFrequencyScore(int n) {
         int digit = 0;
-        int reversed_num = 0;
         while (n > 0){
             digit += n % 10;
-            reversed_num = (reversed_num * 10) + digit ;
             n = n / 10;
         }
         return digit;     
