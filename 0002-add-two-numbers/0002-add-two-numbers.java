e5966aaf1b90c4fa0975a92c095b8065e8bac98e
Aditya Alibade <alibadeaditya@gmail.com>
2026-08-21 23:37:50 +0530

    Time: 1 ms (100%), Space: 46.7 MB (19.77%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -8,44 +8,27 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-import java.math.BigInteger;
+
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode curr1 = l1;
-        ListNode curr2 = l2;
-        String first = "";
-        String second = "";
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        int carry = 0;
 
-        while (curr1 != null) {
-            first += curr1.val;
-            curr1 = curr1.next;
-        }
-        while (curr2 != null) {
-            second += curr2.val;
-            curr2 = curr2.next;
-        }
-
-        first = new StringBuilder(first).reverse().toString();
-        second = new StringBuilder(second).reverse().toString();
-        BigInteger Fno = new BigInteger(first);
-        BigInteger Sno = new BigInteger(second);
-        BigInteger sum = Fno.add(Sno);
-
-        String resultString = new StringBuilder(sum.toString())
-                .reverse()
-                .toString();
-        int[] digits = resultString
-                .chars()
-                .map(c -> c - '0')
-                .toArray();
-
-        ListNode result = new ListNode(digits[0]);
-        ListNode current = result;
-        for (int i = 1; i < digits.length; i++) {
-            current.next = new ListNode(digits[i]);
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = carry;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            current.next = new ListNode(sum % 10);
+            carry = sum / 10;
             current = current.next;
         }
-
-        return result;
+        return dummy.next;
     }
 }
