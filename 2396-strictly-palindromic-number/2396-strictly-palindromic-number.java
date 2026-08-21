e4511ad9adb310ef843be48e4880044b6a34f585
Aditya Alibade <alibadeaditya@gmail.com>
2026-08-21 22:21:02 +0530

    Time: 3 ms (5.99%), Space: 41.7 MB (97.6%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
@@ -1,5 +1,25 @@
 class Solution {
     public boolean isStrictlyPalindromic(int n) {
-        return false;
+        for (int base = 2; base <= n - 2; base++) {
+            int num = n;
+            String str = "";
+
+            while (num > 0) {
+                str = (num % base) + str;
+                num = num / base;
+            }
+
+            int left = 0;
+            int right = str.length() - 1;
+
+            while (left < right) {
+                if (str.charAt(left) != str.charAt(right)) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+        }
+        return true;
     }
 }
